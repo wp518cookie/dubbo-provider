100abc2d248757ccc0a68b19e4bca36e35c294cd
wp <403704156@qq.com>
2019-01-07 15:36:28 +0800

    initial

--- dubbo-provider-server/src/main/java/com/ping/wu/Bootstrap.java
+++ dubbo-provider-server/src/main/java/com/ping/wu/Bootstrap.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author wuping
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ImportResource("classpath:applicationContext.xml")
+@PropertySource("classpath:application.properties")
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class);
